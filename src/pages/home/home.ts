c85c0a0054340f2187b86bf7a1b3233fea8b54bc
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2019-03-01 14:22:21 -0600

    chats id on view replaced with real info

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,9 @@ import { NavController, MenuController } from 'ionic-angular';
 import { ContactsPage } from './../contacts/contacts';
 
 import { ChatProvider } from './../../providers/chat/chat';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+import firebase from 'firebase';
 
 
 
@@ -16,29 +19,46 @@ export class HomePage {
 
   chats: Array<any>;
   allChats = [];
+  recipient: any;
+  user_id: any;
+  chats_ready: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public menuCtrl: MenuController,
-    public chatProvider: ChatProvider
-    ) {
-      this.chatProvider.getChats().subscribe(chat => {
-        console.log(chat);
-        this.chats = chat[1].slice();
-        this.chats = this.chats.concat(chat[0]);
-      });
+    public chatProvider: ChatProvider,
+    private firestore: AngularFirestore
+  ) {
+    this.user_id = firebase.auth().currentUser.uid;
+    this.chatProvider.getChats().subscribe(chat => {
+      console.log(chat);
+      this.chats = chat[1].slice();
+      this.chats = this.chats.concat(chat[0]);
+
+      this.chats.forEach(chat => {
+        let user_recipient = (chat.recipient_id == this.user_id) ? chat.user_id : chat.recipient_id;
+  
+        this.firestore.collection('users/', ref => ref.where('id_user', '==', user_recipient)).valueChanges().subscribe(res => {
+          chat.user = res[0];
+        });
+      })
+      this.chats_ready = true;
+
+    });
+
+    
 
   }
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.menuCtrl.enable(false, 'myMenu');
   }
 
-  openContacts(){
+  openContacts() {
     this.navCtrl.push(ContactsPage)
   }
 
-  openChat(_chat){
+  openChat(_chat) {
     console.log(_chat)
     this.navCtrl.push(ChatPage, {
       _chat: _chat
