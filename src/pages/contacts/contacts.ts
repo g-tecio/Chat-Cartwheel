f0a076f89689260bd644691472de5e7dabedf256
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2019-03-06 11:43:52 -0600

    new chat fixed

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -5,6 +5,8 @@ import { map } from 'rxjs/operators';
 
 import { AuthProvider } from './../../providers/auth/auth';
 import { ChatProvider } from './../../providers/chat/chat';
+import { ChatPage } from '../chat/chat';
+import { HomePage } from '../home/home';
 
 
 @IonicPage()
@@ -68,7 +70,45 @@ export class ContactsPage {
   }
 
   openChat(contact){
-    this.chatProvider.initializeChat(contact);
+    let currentUserID = this.authService.afAuth.auth.currentUser.uid;
+    this.db.collection('chats', ref => ref.where('recipient_id', '==', contact.id_user).where('user_id','==', currentUserID)).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    ).subscribe(chats => {
+      console.log(chats);
+       if(chats.length > 0) {
+        console.log("lo encontre en la primera");
+        this.showSearchbar = false;
+        this.navCtrl.push(ChatPage, {
+          _chat: chats[0]
+        })
+       } else {
+        this.db.collection('chats', ref => ref.where('user_id', '==', contact.id_user).where('recipient_id','==', currentUserID)).snapshotChanges().pipe(
+          map(actions => actions.map(a => {
+            const data = a.payload.doc.data() as any;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          }))
+        ).subscribe(chats => {
+          console.log(chats);
+           if(chats.length > 0) {
+            console.log("lo encontre en la segunda");
+            this.showSearchbar = false;
+            this.navCtrl.push(ChatPage, {
+              _chat: chats[0]
+            })
+           } else {
+            console.log("no lo encontre");
+            this.chatProvider.initializeChat(contact);
+            this.navCtrl.setRoot(HomePage);
+           }
+        })
+       }
+    })
+    //this.chatProvider.initializeChat(contact);
     //this.navCtrl.push(ChatPage);
   }
 
